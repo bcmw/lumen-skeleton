c247dec4d3b109764c64f2d3fb5b457883801977
Alex Galletti <agalletti@bluechipww.com>
2015-10-01 14:47:23 -0500

    error checking for dotenv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-Dotenv::load(__DIR__.'/../');
+try {
+    Dotenv::load(__DIR__.'/../');
+} catch (Exception $e) {
+    exit($e->getMessage());
+}
 
 /*
 |--------------------------------------------------------------------------
